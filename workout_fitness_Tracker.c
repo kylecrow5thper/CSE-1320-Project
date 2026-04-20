89ac8abb602f96fb164b64ea1f0c208383092125
kylecrow5thper <97193019+kylecrow5thper@users.noreply.github.com>
2026-04-19 20:48:58 -0500

    calc PR function done. Kyle-1

--- workout_fitness_Tracker.c
+++ workout_fitness_Tracker.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
- 
+struct Workout {
+    char exercise[30];
+    int weight;
+    int reps;
+};
 
-    return 0;
+struct Workout workouts[100];
+char weeklyPlan[7][50];
+int count = 0;
+
+void calculatePRs(){
+int prB = 0;
+int prS = 0;
+int prD = 0;
+
+    for(int i = 0; i < count; i++){
+        if(strcmp(workouts[i].exercise,"bench") == 0){
+            if(workouts[i].weight > prB){
+            prB = workouts[i].weight;
+            }
+        }
+        else if(strcmp(workouts[i].exercise,"squat") == 0){
+            if(workouts[i].weight > prS){
+            prS = workouts[i].weight;
+            }
+        }
+        else if(strcmp(workouts[i].exercise,"deadlift") == 0){
+            if(workouts[i].weight > prD){
+            prD = workouts[i].weight;
+            }
+        }
+
+       
+    }
+printf("Your bench pr is %d\n", prB);
+printf("Your squat pr is %d\n", prS);
+printf("Your deadlift pr is %d\n", prD);
 }
+
+
